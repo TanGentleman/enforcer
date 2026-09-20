7c86e475f01fc09ba550cf7aa7d51b478a7d47d1
Tanuj <tvasudev@alumni.cmu.edu>
2026-09-20 09:53:35 -0700

    add lock

--- src/root.zig
+++ src/root.zig
@@ -15,6 +15,19 @@ pub fn setHooks(allocator: std.mem.Allocator, io: std.Io, settings: hookSettings
     var dir = try Io.Dir.cwd().openDir(io, dir_path, .{ .follow_symlinks = false });
     defer dir.close(io);
     std.log.info("f: {s}", .{filename});
+
+    // get lock (rn its posix only)
+    var lock_name_buf: [std.fs.max_name_bytes]u8 = undefined;
+    const lock_name = try std.fmt.bufPrint(&lock_name_buf, "{s}.lock", .{filename});
+    var lock_file = try dir.createFile(io, lock_name, .{
+        .read = true,
+        .lock = .exclusive,
+        .truncate = false,
+        .lock_nonblocking = true,
+        .permissions = .fromMode(0o600),
+    });
+    defer lock_file.close(io);
+
     _ = allocator;
     std.debug.print("setting hook to {}\n", .{settings.UserPromptSubmit});
     // postcondition: ~/.claude/settings.json sets hooks accordingly (no duplicates)
